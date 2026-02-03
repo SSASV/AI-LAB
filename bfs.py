4ed823c0b89df42324a3aef3f7f477ebb9b2b079
SSASV <satyavarada05@gmail.com>
2026-02-03 09:18:54 +0530

    Create bfs.py

---
+++ bfs.py
@@ -0,0 +1,43 @@
+#bfs
+def bfs_shortest_path(graph, start, goal):
+    visited = set()
+    queue = [(start, [start])]
+
+    while queue:
+        vertex, path = queue.pop(0)
+
+        if vertex == goal:
+            return path
+
+        if vertex not in visited:
+            visited.add(vertex)
+
+            for neighbor in graph.get(vertex, []):
+                if neighbor not in visited:
+                    queue.append((neighbor, path + [neighbor]))
+
+    return None
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': [],
+    'D': ['G'],
+    'E': ['F'],
+    'F': ['H'],
+    'G': [],
+    'H': ['I', 'J'],
+    'I': [],
+    'J': ['K'],
+    'K': ['L'],
+    'L': []
+}
+
+start = 'A'
+goal = 'H'
+
+path = bfs_shortest_path(graph, start, goal)
+
+if path:
+    print("Shortest path:", " -> ".join(path))
+else:
+    print("No path found")
